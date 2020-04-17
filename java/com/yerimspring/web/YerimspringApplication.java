086ce25de70ce19314a3f261400d3bf326194f79
kangyerim <yerimkang521@gmail.com>
2020-04-17 18:12:47 +0900

    first commit

---
+++ java/com/yerimspring/web/YerimspringApplication.java
@@ -0,0 +1,13 @@
+package com.yerimspring.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YerimspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YerimspringApplication.class, args);
+	}
+
+}
